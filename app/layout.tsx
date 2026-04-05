94409f86071a482fa1dc54c5ef1adb6d786c047e
rladus <knyeon26@gmail.com>
2026-04-05 16:23:39 +0900

    fix: 빌드 에러(대소문자)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 import {
-  ColorSchemeScript,
-  MantineProvider,
-  mantineHtmlProps,
+    ColorSchemeScript,
+    MantineProvider,
+    mantineHtmlProps,
 } from '@mantine/core';
 import '@mantine/core/styles.css';
 import { Analytics } from '@vercel/analytics/next';
@@ -13,54 +13,54 @@ import { THEME } from '@/shared/common/theme';
 import './globals.css';
 
 export const metadata: Metadata = {
-  metadataBase: new URL(SITE_CONFIG.domain),
-  title: {
-    template: '%s | 블랭킷 포크',
-    default: SITE_CONFIG.title,
-  },
-  description: SITE_CONFIG.description,
-  openGraph: {
-    title: SITE_CONFIG.title,
+    metadataBase: new URL(SITE_CONFIG.domain),
+    title: {
+        template: '%s | 블랭킷 포크',
+        default: SITE_CONFIG.title,
+    },
     description: SITE_CONFIG.description,
-    siteName: SITE_CONFIG.title,
-    type: 'website',
-    images: [
-      {
-        url: SITE_CONFIG.thumbnail,
-        width: 1200,
-        height: 630,
-        alt: SITE_CONFIG.image_alt,
-      },
-    ],
-  },
-  icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon-16x16.png',
-    apple: '/apple-touch-icon.png',
-  },
-  other: {
-    google: 'notranslate',
-  },
+    openGraph: {
+        title: SITE_CONFIG.title,
+        description: SITE_CONFIG.description,
+        siteName: SITE_CONFIG.title,
+        type: 'website',
+        images: [
+            {
+                url: SITE_CONFIG.thumbnail,
+                width: 1200,
+                height: 630,
+                alt: SITE_CONFIG.image_alt,
+            },
+        ],
+    },
+    icons: {
+        icon: '/favicon.ico',
+        shortcut: '/favicon-16x16.png',
+        apple: '/apple-touch-icon.png',
+    },
+    other: {
+        google: 'notranslate',
+    },
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang='en' {...mantineHtmlProps}>
-      <head>
-        <ColorSchemeScript />
-      </head>
-      <body>
-        <MantineProvider theme={THEME}>
-          <Header />
-          {children}
-          <Footer />
-        </MantineProvider>
-        <Analytics />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" {...mantineHtmlProps}>
+            <head>
+                <ColorSchemeScript />
+            </head>
+            <body>
+                <MantineProvider theme={THEME}>
+                    <Header />
+                    {children}
+                    <Footer />
+                </MantineProvider>
+                <Analytics />
+            </body>
+        </html>
+    );
 }
